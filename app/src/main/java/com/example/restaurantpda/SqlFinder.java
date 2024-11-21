1395ad97ed7e70e9cfe8bed270903c39e94c48da
JordanKef <kefjordan@gmail.com>
2024-11-21 16:18:55 +0200

    V 1.3 With SQL DB

---
+++ app/src/main/java/com/example/restaurantpda/SqlFinder.java
@@ -0,0 +1,33 @@
+package com.example.restaurantpda;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqlFinder extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "restaurant.db"; // Όνομα βάσης
+    private static final int DATABASE_VERSION = 1; // Έκδοση βάσης
+
+    public SqlFinder(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Δημιουργία πίνακα προϊόντων
+        db.execSQL("CREATE TABLE items (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "tableNumber TEXT, " +
+                "name TEXT, " +
+                "quantity INTEGER, " +
+                "category TEXT, " +
+                "comment TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS items");
+        onCreate(db);
+    }
+}
